9e8d2a89fb64b31eb40583fb1db63a45ff05e2c7
SUNZHIYUAN0102 <zhiyuansun703@gmail.com>
2022-07-06 03:02:27 +0700

    mini app project using vant

---
+++ store/store.js
@@ -0,0 +1,21 @@
+import {
+    action,
+    observable
+} from 'mobx-miniprogram'
+
+export const store = observable({
+    numA: 1,
+    numB: 2,
+
+    get sum() {
+        return this.numA + this.numB
+    },
+
+    updateNum1: action(function(step){
+        this.numA += step
+    }),
+
+    updateNum2: action(function(step){
+        this.numB += step
+    })
+})
